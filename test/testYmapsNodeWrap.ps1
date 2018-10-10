25fd2e013b5288ce12f5c34b48992c5a790e6893
zen_leo <leo.in.empty@gmail.com>
2018-10-10 17:01:06 +0300

    Добавлен Dockerfile для сборки образа

---
+++ test/testYmapsNodeWrap.ps1
@@ -0,0 +1,18 @@
+﻿Clear-Host
+
+$pointsFromArray = 'Москва','Ростов-на-Дону','Батайск','Абаза','Арзамас','Белозерск','Беслан','Буйнакск'
+$pointsToArray = 'Краснодар','Азов','Кемерово','Абакан','Аксай','Барнаул','Волжск'
+
+
+
+foreach ($pointFrom in $pointsFromArray){
+
+    foreach ($pointTo in $pointsToArray){
+    
+    
+    $res = Invoke-WebRequest -Uri ('http://localhost:3000/route?from='+$pointFrom+'&'+'to='+$pointTo)
+    Write-Host $res.Content
+    
+    }
+
+}
