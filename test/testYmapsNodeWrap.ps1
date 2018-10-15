c2e3357a2f845a9a5d20cf97887c3b05433ec781
zen_leo <leo.in.empty@gmail.com>
2018-10-15 10:33:09 +0300

    Имзменен адрес хоста и порт

--- test/testYmapsNodeWrap.ps1
+++ test/testYmapsNodeWrap.ps1
@@ -1,5 +1,9 @@
 ﻿Clear-Host
 
+#$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+#[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+
 $pointsFromArray = '55.811511,37.312518','Ростов-на-Дону','Батайск','Абаза','Арзамас','Белозерск','Беслан','Буйнакск'
 $pointsToArray = 'Краснодар','Азов','Кемерово','Абакан','Аксай','Барнаул','Волжск'
 
@@ -9,7 +13,7 @@ foreach ($pointFrom in $pointsFromArray){
 
     foreach ($pointTo in $pointsToArray){
 
-        $res = Invoke-WebRequest -Uri ('http://localhost:8080/route?apikey=IQTCgkwwGXEIGNtwka6J3li5xg2G8Ds1&waypoints=' + $pointFrom + '|' + $pointTo)
+        $res = Invoke-WebRequest -Uri ('https://localhost:8080/route?apikey=IQTCgkwwGXEIGNtwka6J3li5xg2G8Ds1&waypoints=' + $pointFrom + '|' + $pointTo)
         Write-Host $res.Content
     
     }
