b2f61adce619c3d9bdfc59e1939d59f8cb13ffd4
Alexander <strilchukalexander@gmail.com>
2018-10-15 15:33:31 +0300

    123

--- routes/route.js
+++ routes/route.js
@@ -10,7 +10,9 @@ var browser, page;
 
 (async ()=>{
 
-    browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+    //browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+    browser = await puppeteer.launch();
+
     page = await browser.newPage();
     const htmlWrapPath = path.join(path.dirname(__dirname), 'html_wrap');
     await page.goto('file:///' + path.join(htmlWrapPath, 'apiWrap.html'));
